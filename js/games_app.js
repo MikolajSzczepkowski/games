118fc4f7d82bc0bd3d56909a70b6e71c4c5f8f2c
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-01-12 11:27:29 +0100

    support test

--- js/games_app.js
+++ js/games_app.js
@@ -21,20 +21,25 @@ $(function (){
 		$(document).on("mouseleave", ".topic-inner-wrapper", function(){
 			currentTeamTopic.find(".enter-topic").nextAll().fadeOut(200);		
 		});
+		$(document).on("click", ".topic-list", function(){
+			clickedTopic = $(this).text();
+			currentTeamTopic.find(".enter-topic").nextAll().fadeOut(200);
+			currentTeamTopic.find(".enter-topic").empty().css({"padding-top":"5px",
+																"background":"#363636"}).text(clickedTopic);	
+		});
 	}
 	else{
 		$(document).on("click", ".enter-topic", function(){
 			currentTeamTopic = $(this);
 			currentTeamTopic.nextAll().fadeIn(200);
 		});
+		$(document).on("click", ".topic-list", function(){
+			clickedTopic = $(this).text();
+			currentTeamTopic.nextAll().fadeOut(200);
+			currentTeamTopic.empty().css({"padding-top":"5px",
+																"background":"#363636"}).text(clickedTopic);	
+		});
 	}
-	
-	$(document).on("click", ".topic-list", function(){
-		clickedTopic = $(this).text();
-		currentTeamTopic.find(".enter-topic").nextAll().fadeOut(200);
-		currentTeamTopic.find(".enter-topic").empty().css({"padding-top":"5px",
-															"background":"#363636"}).text(clickedTopic);	
-	});
 
 	$(document).on("mouseenter", ".result-inner-wrapper", function(){
 		currentTeamResult = $(this);
